6d33207c50a93ca33187d23a3768d537fb580b54
abrorbek_anvarbekov <1aabror@gmail.com>
2023-04-26 03:28:25 +0900

    article regDate 기능 구현

--- src/Basic_Articile_Manager/Main.java
+++ src/Basic_Articile_Manager/Main.java
@@ -1,4 +1,8 @@
+package Basic_Articile_Manager;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -8,6 +12,7 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         List<Article> articleList = new ArrayList<>();
+
         int articlesId = 0;
         while (true) {
             System.out.println("명령어)");
@@ -24,7 +29,7 @@ public class Main {
                 String title = sc.nextLine();
                 System.out.println("제목  :   ");
                 String body = sc.nextLine();
-
+//                String regDate = Util.getDateStr();
                 Article article = new Article(id, title, body);
                 articleList.add(article);
                 System.out.printf("%d 번 글이 생성되었습니다.\n", id);
@@ -36,10 +41,10 @@ public class Main {
                 }
 
                 System.out.println("=== 게시물 목록 ===");
-                System.out.println("번호  |   제목  ");
+                System.out.println("번호  |   제목  |   작성일   ");
                 for (int i = articleList.size() - 1; i >= 0; i--) {
                     Article article = articleList.get(i);
-                    System.out.printf("%d   |   %s  \n", article.id, article.title);
+                    System.out.printf("%d   |   %s  |   %s\n", article.id, article.title, article.regDate);
                 }
             } else if (cmd.startsWith("article detail ")) {
                 String[] cmdBits = cmd.split(" ");
@@ -60,6 +65,7 @@ public class Main {
                 System.out.printf("번호   |   %d\n", foundArticle.id);
                 System.out.printf("제목   |   %s\n", foundArticle.title);
                 System.out.printf("내용   |   %s\n", foundArticle.body);
+                System.out.printf("시간   |   %s\n", foundArticle.regDate);
             }else {
                 System.out.println("존재하지 않는 명령어 입니다!!");
             }
@@ -74,8 +80,14 @@ class Article {
     int id;
     String title;
     String body;
-
+    static String regDate;
+    public Article(){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy년 MM월 dd날 HH시 mm분 ss초");
+        Date now = new Date();
+        regDate = formatter.format(now);
+    }
     public Article(int id, String title, String body) {
+        this();
         this.id = id;
         this.title = title;
         this.body = body;
---
+++ src/Basic_Articile_Manager/Util.java
@@ -0,0 +1,12 @@
+package Basic_Articile_Manager;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+    public static String getDateStr(){
+        SimpleDateFormat formater = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date now = new Date();
+        return formater.format(now);
+    }
+}
