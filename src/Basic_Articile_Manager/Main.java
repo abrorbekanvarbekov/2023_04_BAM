c52b08d556247e2f186e1372a8f406cc6737795a
abrorbek_anvarbekov <1aabror@gmail.com>
2023-04-26 04:08:23 +0900

    article 수정, 삭제 기능 구현

--- src/Basic_Articile_Manager/Main.java
+++ src/Basic_Articile_Manager/Main.java
@@ -1,5 +1,6 @@
 package Basic_Articile_Manager;
 
+import java.awt.geom.Arc2D;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -66,7 +67,45 @@ public class Main {
                 System.out.printf("제목   |   %s\n", foundArticle.title);
                 System.out.printf("내용   |   %s\n", foundArticle.body);
                 System.out.printf("시간   |   %s\n", foundArticle.regDate);
-            }else {
+                
+            } else if (cmd.startsWith("article modify ")) {
+                String[] cmdBist = cmd.split(" ");
+                int id = Integer.parseInt(cmdBist[2]);
+                Article foundArticle = null;
+                for (Article article: articleList) {
+                    if (article.id == id){
+                        foundArticle = article;
+                        break;
+                    }
+                }
+
+                if (foundArticle == null){
+                    System.out.printf("%d번 개시물은 존재하지 않습니다!\n", id);
+                    continue;
+                }
+                System.out.println("=== 게시물 수정 ===");
+                System.out.printf("수정할 제목   :\n");
+                String title = sc.nextLine();
+                System.out.printf("수정할 내용   :\n");
+                String body = sc.nextLine();
+
+                foundArticle.title = title;
+                foundArticle.body = body;
+            } else if (cmd.startsWith("article delete ")) {
+                String[] cmdBist = cmd.split(" ");
+                int id = Integer.parseInt(cmdBist[2]);
+
+                for (Article article : articleList){
+                    if (article.id == id){
+                        articleList.remove(article);
+                        System.out.printf("%d번 개시물이 삭제 되었습니다!\n", id);
+                        break;
+                    }else {
+                        System.out.printf("%d번 개시물은 존재하지 않습니다!\n", id);
+                        continue;
+                    }
+                }
+            } else {
                 System.out.println("존재하지 않는 명령어 입니다!!");
             }
 
