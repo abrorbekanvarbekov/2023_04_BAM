c66fd6c6b4d9056e8ebe2e2ed4fa8bf25c317368
abrorbek_anvarbekov <1aabror@gmail.com>
2023-04-26 17:39:56 +0900

    date tesãt ê¸기능 구현

--- src/Basic_Articile_Manager/Main.java
+++ src/Basic_Articile_Manager/Main.java
@@ -1,6 +1,5 @@
 package Basic_Articile_Manager;
 
-import java.awt.geom.Arc2D;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -8,13 +7,19 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    static List<Article> articleList;
+    static int articlesId;
+
+    static {
+        articleList = new ArrayList<>();
+        articlesId = 0;
+    }
+
     public static void main(String[] args) {
         System.out.println("== 프로그램 시작 ==");
-
+        maketestDate();
         Scanner sc = new Scanner(System.in);
-        List<Article> articleList = new ArrayList<>();
 
-        int articlesId = 0;
         while (true) {
             System.out.println("명령어)");
             String cmd = sc.nextLine();
@@ -30,8 +35,8 @@ public class Main {
                 String title = sc.nextLine();
                 System.out.println("제목  :   ");
                 String body = sc.nextLine();
-//                String regDate = Util.getDateStr();
-                Article article = new Article(id, title, body);
+                String regDate = Util.getDateStr();
+                Article article = new Article(id, regDate ,title, body);
                 articleList.add(article);
                 System.out.printf("%d 번 글이 생성되었습니다.\n", id);
 
@@ -57,7 +62,7 @@ public class Main {
                         break;
                     }
                 }
-                if (foundArticle == null){
+                if (foundArticle == null) {
                     System.out.printf("%d 번 개시물은 존재하지 않습니다.\n", id);
                     continue;
                 }
@@ -67,19 +72,19 @@ public class Main {
                 System.out.printf("제목   |   %s\n", foundArticle.title);
                 System.out.printf("내용   |   %s\n", foundArticle.body);
                 System.out.printf("시간   |   %s\n", foundArticle.regDate);
-                
+
             } else if (cmd.startsWith("article modify ")) {
                 String[] cmdBist = cmd.split(" ");
                 int id = Integer.parseInt(cmdBist[2]);
                 Article foundArticle = null;
-                for (Article article: articleList) {
-                    if (article.id == id){
+                for (Article article : articleList) {
+                    if (article.id == id) {
                         foundArticle = article;
                         break;
                     }
                 }
 
-                if (foundArticle == null){
+                if (foundArticle == null) {
                     System.out.printf("%d번 개시물은 존재하지 않습니다!\n", id);
                     continue;
                 }
@@ -94,17 +99,19 @@ public class Main {
             } else if (cmd.startsWith("article delete ")) {
                 String[] cmdBist = cmd.split(" ");
                 int id = Integer.parseInt(cmdBist[2]);
-
-                for (Article article : articleList){
-                    if (article.id == id){
-                        articleList.remove(article);
-                        System.out.printf("%d번 개시물이 삭제 되었습니다!\n", id);
+                Article foundArticle = null;
+                for (Article article : articleList) {
+                    if (article.id == id) {
+                        foundArticle = article;
                         break;
-                    }else {
-                        System.out.printf("%d번 개시물은 존재하지 않습니다!\n", id);
-                        continue;
                     }
                 }
+                if(foundArticle == null){
+                    System.out.printf("%d번 개시물은 존재하지 않습니다!\n", id);
+                    continue;
+                }
+                articleList.remove(foundArticle);
+                System.out.printf("%d번 게시물이 삭제 되었습니다!\n", id);
             } else {
                 System.out.println("존재하지 않는 명령어 입니다!!");
             }
@@ -113,20 +120,35 @@ public class Main {
         sc.close();
         System.out.println("=== 프로그램 끝 ===");
     }
+
+    private static void maketestDate() {
+        System.out.println("테스트용 게시물 데이터 5 개 새성");
+        for (int i = 1; i <= 5; i++) {
+            int id = articlesId + 1;
+            articlesId = id;
+            String title = "제목" + i;
+            String body = "내용" + i;
+            Article article = new Article(id, Util.getDateStr(), title, body);
+            articleList.add(article);
+        }
+    }
 }
 
 class Article {
     int id;
     String title;
     String body;
-    static String regDate;
-    public Article(){
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy년 MM월 dd날 HH시 mm분 ss초");
-        Date now = new Date();
-        regDate = formatter.format(now);
-    }
-    public Article(int id, String title, String body) {
-        this();
+    String regDate;
+
+//    public Article(int id, String dateStr, String title, String body) {
+//        SimpleDateFormat formatter = new SimpleDateFormat("yyyy년 MM월 dd날 HH시 mm분 ss초");
+//        Date now = new Date();
+//        regDate = formatter.format(now);
+//    }
+
+    public Article(int id, String regDate, String title, String body) {
+//        this(id, Util.getDateStr(), title, body);
+        this.regDate = regDate;
         this.id = id;
         this.title = title;
         this.body = body;
